4f35f0edb8d84fb0d3564b1553648e418b69432e
Oscar Randall <z5592043@ad.unsw.edu.au>
2025-09-26 17:39:18 +1000

    Oscar Week 1

--- phys3116_comp.py
+++ phys3116_comp.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # read Harris Part I Data
-harris_part1 = r"C:\Users\lachl\phys_3116_data\HarrisPartI.clv"
+harris_part1 =pd.read_csv(r"C:\Users\oscar\Downloads\HarrisPartI (1).csv")
 
 # read Harris Part 2 Data
+hariss_part2 =pd.read_csv(r"C:\Users\oscar\Downloads\HarrisPartIII.csv")
 
+# read Krasue 21 Data
+Krasue21 =pd.read_csv(r"C:\Users\oscar\Downloads\Krause21.csv")
 
-# read Krause21 Data
-
-
-# read VandenBerg table 2
+# read vandenBerg_Table2
+vanderberg_table2 =pd.read_csv(r"C:\Users\oscar\Downloads\vandenBerg_table2.csv")

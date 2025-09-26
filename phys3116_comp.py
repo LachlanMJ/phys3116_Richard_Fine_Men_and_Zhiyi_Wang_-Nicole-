f39d4c55b36927542d9c0e53b2c3deac866e8892
nicole <nicolezywang@gmail.com>
2025-09-26 22:46:29 +1000

    scatter plot

--- phys3116_comp.py
+++ phys3116_comp.py
@@ -13,3 +13,14 @@ Krasue21 =pd.read_csv(r"C:\Users\oscar\Downloads\Krause21.csv")
 
 # read vandenBerg_Table2
 vanderberg_table2 =pd.read_csv(r"C:\Users\oscar\Downloads\vandenBerg_table2.csv")
+
+# scatter plot Krasue 21 Data
+plt.scatter(de['Age'], df['Fe'])
+
+#Add labels and titles for the plot
+plt.xlabel('Age')
+plt.ylabel('fe(Iron)')
+plt.title('Fe vs Age')
+
+#Show plot
+plt.show()
